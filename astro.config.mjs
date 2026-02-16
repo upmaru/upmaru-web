eeaaafb8f263a0e6ba21308c0ee3567c8e0f146a
Zack Siri <me@zacksiri.com>
2026-02-16 20:21:27 +0700

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import rehypeMermaid from "rehype-mermaid";
 
 export default defineConfig({
+  site: "https://upmaru.com",
   integrations: [react()],
   markdown: {
     syntaxHighlight: {
